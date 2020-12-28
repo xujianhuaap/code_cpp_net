a149d13f1f0388ebf2c1b59429a54cb0f9fb3a7c
skullmind <xujianhuaap@gmail.com>
2020-12-28 17:39:38 +0800

    feature:cmakelist add deb package

---
+++ deb.cmake
@@ -0,0 +1,14 @@
+############################# CPACK START###############
+
+set(CPACK_GENERATOR "DEB")
+set(CPACK_PACKAGE_VERSION "1.0.0")
+set(CPACK_SET_DESTDIR ON)
+set(CPACK_DEB_PACKAGE_GROUP "DEV")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "SkullMind")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "")
+
+include(CPack)
+#cpack_add_component_group(DEV)
+#cpack_add_component(tool GROUP DEV)
+
+########### CPACK END###################################
---
+++ post.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ project_dir="$(pwd)"
+ cd "${project_dir}/build"
+make package
