68f6bc79a7c81d4e3d440cdd1626cfea3bfd87e6
skullmind <xujianhuaap@gmail.com>
2020-12-29 22:23:35 +0800

    feature[package.sh]:clean build before build

--- package.sh
+++ package.sh
@@ -7,6 +7,12 @@ cmake_path=/home/xujianhua/Documents/soft/clion-2019.3.3/bin/cmake/linux/
 cpack_config_file="${project_build}/CPackConfig.cmake"
 cpack_source_config_file="${project_build}/CPackSourceConfig.cmake"
 
+# clean build
+if test -d "${project_build}";then
+  echo "---> clean build"
+  rm -rf ${project_build}
+fi
+
 # build project
 ${cmake_path}/bin/cmake -DCMAKE_BUILD_TYPE=Debug\
   -DMAKE_BUILD_TYPE=Debug -DCOMPILE_TESTS=ON\
